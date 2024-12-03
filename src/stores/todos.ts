651fbb5f15ead3109819bb7700ebcb2c8339651b
eruzanov <eugene.ruzanov@yandex.ru>
2024-12-03 19:39:03 +0300

    добавляет синхронизацию с localStorage

--- src/stores/todos.ts
+++ src/stores/todos.ts
@@ -1,10 +1,12 @@
-import { ref } from 'vue';
 import { defineStore } from 'pinia';
 import { v4 as uuidv4 } from 'uuid';
+import { useStorage } from '@vueuse/core';
 import type { Todo } from './todo.type';
 
-export const useTodosStore = defineStore('todos', () => {
-  const todos = ref<Todo[]>([]);
+const LABEL_STORE = 'todos';
+
+export const useTodosStore = defineStore(LABEL_STORE, () => {
+  const todos = useStorage<Todo[]>(LABEL_STORE, []);
 
   function add(name: Todo['name']) {
     todos.value = todos.value.concat({ id: uuidv4(), name });
